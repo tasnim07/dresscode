665393d75adfcfe14c0eed64cf0919d9965b6131
Tasneem <tasnim07@gmail.com>
2016-06-03 02:20:50 +0530

    app.models: created Item, category, brand and cart model | app.migrations: added migration files for the new models

--- app/models.py
+++ app/models.py
@@ -15,3 +15,56 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    thumbnail = models.URLField(blank=True, null=True)
+    published_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey('Category', related_name='items')
+    brand = models.ForeignKey('Brand', null=True, related_name='items')
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    item = models.ForeignKey('Item', related_name='carts')
+    user = models.ForeignKey('UserProfile', related_name='carts')
+
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+
+    def __str__(self):
+        return '--'.join([self.item.name, self.user.username])
