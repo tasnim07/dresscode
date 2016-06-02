3df91d0f66b4a0813f96ab01e9194dc87ccb0379
Tasneem <tasnim07@gmail.com>
2016-06-03 01:08:26 +0530

    Create user profile model and migration files for the same | register user in admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'avatar', 'address']
+    search_fields = ['user']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.URLField(blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True,
+                                verbose_name='Phone Number')
+    address = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'UserProfile'
+        verbose_name_plural = 'UserProfiles'
+
+    def __str__(self):
+        return self.user.username
